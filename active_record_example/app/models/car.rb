1aa4c2ec3915601ba196a4f2b0eed98c71a9d474
Joshua Toyota <jearsh@gmail.com>
2015-08-19 01:41:01 -0400

    clerify that the instance variable/attribute on the object is updated when using #{access_as}=

--- active_record_example/app/models/car.rb
+++ active_record_example/app/models/car.rb
@@ -9,12 +9,18 @@ end
 # ---------------------------------------
 #
 # car = Car.first
-# car.report_status.title  # => "Work Log"
+# car.report_status_id     # => 3
 #
 # car.report_status.id     # => 3
+# car.report_status.title  # => "Work Log"
 # car.report_status.name   # => :work_log
 #
+#
 # car.report_status = :completed
+#
+# car.report_status_id     # => 4
+#
+# car.report_status.id     # => 4
 # car.report_status.title  # => "Completed"
 #
 # car.report_status = 2
